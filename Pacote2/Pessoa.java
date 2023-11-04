6f4cb4eafeb215cd214e06cb52ac0478d0bda36b
NatanaelPimenta <143766182+NatanaelPimenta@users.noreply.github.com>
2023-11-04 09:04:24 -0300

    Add files via upload

---
+++ Pacote2/Pessoa.java
@@ -0,0 +1,13 @@
+package Pacote2;
+
+public class Pessoa {
+    String nome;
+    String email;
+    String telefone;
+
+    public Pessoa(String nome, String email, String telefone) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
